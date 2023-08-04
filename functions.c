659d69cfb256ef7ea1646c07eebfb25d14198a48
faithchamp <faithchamp@Ubuntu.myguest.virtualbox.org>
2023-08-04 01:36:56 +0000

    projects competed for the day

--- functions.c
+++ functions.c
@@ -1,28 +1,18 @@
 #include <stdio.h>
-#define NAME "David Jackson, Inc."
-#define ADDRESS "GREATER ACCRA- GHANA"
-#define TELE "055746921"
-#define WIDTH 40
 
-void our_line(void){
-
-	for(int i = 0; i <= WIDTH; i++){
-	printf("-");
-	}
- 	
-	printf("\n");
-}
-
+int is_prime(int)
 int main(void){
-	our_line();
-	                  
-	printf("%s\n", NAME);
-	printf("%s\n", ADDRESS);
-	printf("%s\n", TELE);
-        
-	our_line();	
+
 return 0;
 }
 
+int is_prime(int a){
+	for(int i = 2; i < a; i++){
+		if(a % i == 0){
+			return 1;
+		}
+	}
+	   
 
-
+return 0;	
+}
